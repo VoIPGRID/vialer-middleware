514679e1d1daefa01444d7d03161af8373e83e28
Daniël Niemeijer <daniel_niemeijer@hotmail.com>
2018-04-11 10:30:51 +0200

    Added flake8 packages and fixed several pep errors

--- docs/ci.sh
+++ docs/ci.sh
@@ -2,8 +2,6 @@
 
 set -e
 
-COMPOSE_FILE=docker-compose.yml
-
 # Run CI tests, start by printing a warning.
 echo "Warning, this script docker containers/volumes, hit CTRL+C in 5 secs to abort."
 sleep 5
@@ -11,25 +9,21 @@ sleep 5
 touch .env
 
 # Remove possible previous state.
-sudo docker-compose -f $COMPOSE_FILE kill
-sudo docker-compose -f $COMPOSE_FILE rm -vf
-sudo docker-compose -f $COMPOSE_FILE build
+docker-compose kill
+docker-compose rm -vf
+docker-compose build
 
 # Wait for db service to be fully initialized.
-sudo docker-compose run --rm app ls
+docker-compose run --rm app ls
 sleep 10
 
-# Run the tests.
+# Run the tests, but disable aborting the script on errors.
 set +e
-RESULT=0
-sudo docker-compose -f $COMPOSE_FILE run --rm --service-ports app python manage.py test \
+docker-compose run --rm --service-ports app python manage.py test \
     --with-coverage --cover-package=. --cover-xml --cover-xml-file=coverage.xml \
     --with-xunit --xunit-file=xunit.xml
-EC=$?
-if [ $EC -ne 0 ]; then RESULT+=1; fi
+set -e
 
 # Cleanup.
-sudo docker-compose -f $COMPOSE_FILE kill
-sudo docker-compose -f $COMPOSE_FILE rm -vf
-
-exit $RESULT
+docker-compose kill
+docker-compose rm -vf
