20ff92b1b246f574d37743ac2c64ff3bd621d83b
Jos van Bakel <josvanbakel@protonmail.com>
2019-01-14 09:09:50 +0100

    Use whitenoise for static files

--- main/settings.py
+++ main/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.cache.FetchFromCacheMiddleware',
 )
 
--- main/urls.py
+++ main/urls.py
@@ -1,20 +1,8 @@
-import re
-
-from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
-from django.views.static import serve
 
 
 urlpatterns = [
     url(r'^api/', include('api.urls')),
     url(r'^admin/', include(admin.site.urls)),
-
-    # XXX: Hack for static files, gunicorn can't serve them.
-    #      django explicitly states that using the staticfiles_urlpatterns,
-    #      is *not* suitable for production. But as this is only used for
-    #      admin interface we use it anyway.
-    #      This has been copied from: django.conf.urls.static
-    url(r'^%s(?P<path>.*)$' % re.escape(settings.STATIC_URL.lstrip('/')), serve,
-        kwargs={'document_root': settings.STATIC_ROOT}),
 ]
